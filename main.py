2b54bb8bbc5995b7badb2c49902b7b23e2dc5578
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2025-03-27 20:13:48 +1100

    make comments better

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ node_probabilities = {
 }
 
 def gen_node(level):
+    # generates a node based on probabilites in "node_probabilites", following some rules:
+    # elites, rest sites and merchants can never be adjacent to themselves
+    # we take a list "level" so we can check the last node that was generated for our level
+    # takes the current level list
+    # returns a node
+
     # we loop until we get a valid node
     while True:
         # standard random level picker
@@ -20,7 +26,7 @@ def gen_node(level):
         for node_type, prob in node_probabilities.items():
             cumulative_probability += prob
             if rand <= cumulative_probability:
-                # check if we violate rules of world gen, no adjacent elites, rest sites or merchants
+                # we check the most recent node generated for the current level by taking level[-1] and use it to make sure we arent violating the rules
                 if level[-1] == "elite" or level[-1] == "rest_site" or level[-1] == "merchant":
                     if node_type != level[-1]:
                         # send it now that we have determined it doesnt violate rules
@@ -33,38 +39,40 @@ def gen_node(level):
                     return node_type
 
 def gen_map():
+    # generates a map which consists of 15 nodes, following some rules:
+    # node 1 is always a monster, node 9 is always a treasure, node 14 is never a rest site and node 15 is always a rest site
+    # nodes 1-5 can never be an elite or rest site
+    # returns a list of 15 nodes
+
+    # this is a list containing all the nodes for the current level
     level = []
-    # 15 nodes per level
     for i in range(15):
-        # first node is always a monster
         if i == 0:
             level.append("monster")
-        # first 5 nodes are never an elite or rest site
         elif i < 5:
             node = gen_node(level)
             # retry node gen if we get an elite or rest site
             while node == "elite" or node == "rest_site":
                 node = gen_node(level)
             level.append(node)
-        # 9th node is always treasure
         elif i == 8:
             level.append("treasure")
-        # 14th node is never a rest site because the 15th is always a rest site
         elif i == 13:
             node = gen_node(level)
             # retry node gen if we get a rest site
             while node == "rest_site":
                 node = gen_node(level)
             level.append(node)
-        # 15th node is always a rest site
         elif i == 14:
             level.append("rest_site")
         else:
-            # all other nodes are standardly generated
             level.append(gen_node(level))
     return level
 
 def gen_world():
+    # generates a world (list of 45 nodes which is just 3 levels joined together)
+    # returns a list of 45 nodes (3 levels)
+
     world = []
     # 3 levels per world
     for _ in range(3):
