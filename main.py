45ecf57b9e3881c8e978e611a99a4dc1ed4e5c96
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2025-03-29 01:59:18 +1100

    add colourless

--- main.py
+++ main.py
@@ -173,6 +173,9 @@ games = 1000000
 rare_draws = 0
 uncommon_draws = 0
 common_draws = 0
+colourless_rare_draws = 0
+colourless_uncommon_draws = 0
+colourless_common_draws = 0
 for i in tqdm(range(games)):
     world = gen_world()
     for node in world:
@@ -200,7 +203,7 @@ for i in tqdm(range(games)):
                                 offset += 1
                                 common_draws += 1
             case "merchant":
-                for i in range(7):
+                for i in range(5):
                         match gen_card(merchant_card_probs, offset):
                             case "rare":
                                 rare_draws += 1
@@ -208,9 +211,17 @@ for i in tqdm(range(games)):
                                 uncommon_draws += 1
                             case "common":
                                 common_draws += 1
+                for i in range(2):
+                        match gen_card(merchant_card_probs, offset):
+                            case "rare":
+                                colourless_rare_draws += 1
+                            case "uncommon":
+                                colourless_uncommon_draws += 1
+                            case "common":
+                                colourless_common_draws += 1
             case "boss":
                 offset = -5
                 for i in range(3):
                     rare_draws += 1
 
-print([rare_draws/games, uncommon_draws/games, common_draws/games])
+print([rare_draws/games, uncommon_draws/games, common_draws/games], [colourless_rare_draws/games, colourless_uncommon_draws/games, colourless_common_draws/games])
